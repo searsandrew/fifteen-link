5100ae9a33b2a9cfb72ba8db99620d74685b08c2
Andrew Sears <andrew@mayfifteenth.com>
2024-11-18 17:00:59 -0600

    Update colors and styles

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,15 @@ Route::get('/', function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::middleware('verified')->group(function() {
+    Route::middleware('verified')->group(function () {
         Route::resource('link', App\Http\Controllers\LinkController::class);
         Route::view('/dashboard', 'dashboard')->name('dashboard');
     });
-    Route::resource('profile', App\Http\Controllers\ProfileController::class)->only(['update', 'destroy']);
     Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [App\Http\Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
 Route::get('/{reference}', [App\Http\Controllers\RedirectService::class, 'go'])->name('redirect');
