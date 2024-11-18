38a58e6502b239057ae27bdd8bfcee0e1441f943
Andrew Sears <andrew@mayfifteenth.com>
2024-11-18 13:45:03 -0600

    Add relationships to User and Link models

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Link extends Model
 {
     use HasUlids;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
